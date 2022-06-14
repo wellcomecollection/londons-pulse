1abe7c9ebd921735bbef96a43d18b3ff69aad360
Alex Chan <alex@alexwlchan.net>
2022-06-14 15:22:07 +0100

    Remove the deprecated template_file resource/provider

--- infrastructure/bastion.tf
+++ infrastructure/bastion.tf
@@ -1,10 +1,6 @@
-data "template_file" "public_key" {
-  template = file("files/key.pub")
-}
-
 resource "aws_key_pair" "this" {
   key_name   = "moh"
-  public_key = data.template_file.public_key.rendered
+  public_key = file("files/key.pub")
 
   lifecycle {
     create_before_destroy = true
